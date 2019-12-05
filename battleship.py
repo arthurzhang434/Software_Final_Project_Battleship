124ba1bd294237882fb77a0ba98ae3d9fb31e335
arthurzhang434 <54689336+arthurzhang434@users.noreply.github.com>
2019-12-05 16:56:51 -0500

    Update battleship.py

--- battleship.py
+++ battleship.py
@@ -10,12 +10,33 @@ import pygame
 #initialize the pygame
 pygame.init()
 
+#Set up the game screen size
 screen = pygame.display.set_mode((800,600))
 
 #Title and Icon
 pygame.display.set_caption("Arthur's Battleship")
 Icon = pygame.image.load("battleship.png")
 pygame.display.set_icon(Icon)
+
+#Set up the colors
+WHITE = (255, 255, 255)#Nothing
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)#Hit
+GREEN = (0, 255, 0)#Miss
+BLUE = (0, 0, 255)#Ship
+
+#
+grid = []
+for row in range(10):
+    grid.append([])
+    for column in range(10):
+        grid[row].append(0)
+        
+upgrid = []
+for row in range(10):
+    upgrid.append([])
+    for column in range(10):
+        upgrid[row].append(10)
  
 #game running loop
 running = True
@@ -24,4 +45,6 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             exit()
+            
+
         
